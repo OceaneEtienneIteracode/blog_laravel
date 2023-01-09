d1dcada71299b97e789b1b6edba752f804b55889
Oceane <oceane@MacBook-Air-de-Oceane.local>
2023-01-09 13:26:14 +0100

    second commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1> {!! $post->title !!} </h1>
+    <div>{!! $post->body !!}</div>
+</article>
+<a href="/">Go back</a>
+</body>
